0558d4a64cb031f80986dbb458e7b4056fee7c65
Paul Biermann <pmbiermann@proton.me>
2025-09-30 23:43:47 -0500

    Apply basic styles to CountryCard

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Country } from "../../entities/types";
+import styles from './CountryCard.module.css'
 
 interface CountryCardProps {
     country: Country;
 }
 
 const CountryCard: React.FC<CountryCardProps> = ({ country }) => {
-    return (<div>
-        <img src={country.flags.svg} alt={country.flags.alt} />
-        <h2>{country.name.common}</h2>
-        <p>Population: {country.population}</p>
-        <p>Capital: {country.capital.join(', ')}</p>
-        <p>Region: {country.region}</p>
-    </div>);
+    return <div className={styles['country-card']}>
+        <img src={country.flags.svg} alt={country.flags.alt} className={styles['country-card-img']} />
+        <div className={styles['country-card-contents']}>
+            <h2>{country.name.common}</h2>
+            <p>Population: {country.population}</p>
+            <p>Capital: {country.capital.join(', ')}</p>
+            <p>Region: {country.region}</p>
+        </div>
+        <button className={styles['country-card-button']}>See more</button>
+    </div>;
 }
 
 export default CountryCard;
