8eafe66f607103595616f2523f5db3e502f0b6e4
Paul Biermann <pmbiermann@proton.me>
2025-09-30 23:49:32 -0500

    Create empty CountryDetails component

---
+++ src/components/CountryDetails/CountryDetails.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import CountryDetails from './CountryDetails';
+import { Country } from '../../entities/types';
+
+const mockCountry: Country = {
+    "flags": {
+        "png": "https://flagcdn.com/w320/us.png",
+        "svg": "https://flagcdn.com/us.svg",
+        "alt": "The flag of the United States of America is composed of thirteen equal horizontal bands of red alternating with white. A blue rectangle, bearing fifty small five-pointed white stars arranged in nine rows where rows of six stars alternate with rows of five stars, is superimposed in the canton."
+    },
+    "name": {
+        "common": "United States",
+        "official": "United States of America",
+        "nativeName": {
+            "eng": {
+                "official": "United States of America",
+                "common": "United States"
+            }
+        }
+    },
+    "capital": [
+        "Washington, D.C."
+    ],
+    "region": "Americas",
+    "population": 329484123
+};
+
+describe('CountryDetails test', () => {
+    it('renders placeholder', () => {
+        render(<CountryDetails/>);
+        const linkElement = screen.getByText('CountryDetails Place Holder');
+        expect(linkElement).toBeInTheDocument();
+    });
+});
---
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryDetails: React.FC = () => {
+    return <h2>CountryDetails Place Holder</h2>
+}
+
+export default CountryDetails;
